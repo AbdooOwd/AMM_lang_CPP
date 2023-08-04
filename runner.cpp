4b85e20134138b729fc329d5ec50e91945ac0a7d
AbdooOwd <benari102@gmail.com>
2023-08-04 21:16:20 +0000

    Other Commit

--- runner.cpp
+++ runner.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
-std::vector<std::string> known_keywords = {
-    "whatif",
-    "orelse",
-    "endif",
-    "log"
-};
 
-string get_code(const string& file_path) {
-    ifstream program_file(file_path);
-    return string(istreambuf_iterator<char>(program_file), {});
+string get_code(string file_path, bool per_line = false) {
+
+    ifstream code_file(file_path); // Gets file
+    if (!code_file.is_open()) { // Checks if the file exists
+        cout << "Error opening the file." << endl;
+        return "";
+    }
+
+    if (per_line == false) { // Gets file content
+        std::string content((std::istreambuf_iterator<char>(code_file)),
+        std::istreambuf_iterator<char>());
+        std::cout << content << std::endl;
+    } else {
+        std::string line;
+        while (std::getline(code_file, line)) {
+        // Process the line here
+        std::cout << line << std::endl;
+        }
+    }
+
+}
+
+void run(string file_path) {
+
+    string code = get_code(file_path); // Stores the file content in `code` variable
+
+}
+
+
+int main(int argc, char const *argv[]) {
+    // The command to run the program is "gcc main.cpp -lstdc++ -o main.o"
+    get_code(argv[1]);
+    return 0;
 }

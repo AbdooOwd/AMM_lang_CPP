7958e900bbe9694aca8166b93862f6cf71e7c9a0
AbdooOwd <benari102@gmail.com>
2023-08-04 22:14:45 +0000

    runner.py rewritten in C++

--- runner.cpp
+++ runner.cpp
@@ -1,40 +1,111 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
+vector<string> known_keywords = {
+    "whatif",
+    "orelse",
+    "endif",
+    "log"
+};
 
-string get_code(string file_path, bool per_line = false) {
-
-    ifstream code_file(file_path); // Gets file
-    if (!code_file.is_open()) { // Checks if the file exists
-        cout << "Error opening the file." << endl;
+string get_code(const string& file_path) {
+    ifstream program_file(file_path);
+    if (!program_file.is_open()) {
+        cerr << "Error opening the file." << endl;
         return "";
     }
+    string code((istreambuf_iterator<char>(program_file)), {});
+    return code;
+}
 
-    if (per_line == false) { // Gets file content
-        std::string content((std::istreambuf_iterator<char>(code_file)),
-        std::istreambuf_iterator<char>());
-        std::cout << content << std::endl;
-    } else {
-        std::string line;
-        while (std::getline(code_file, line)) {
-        // Process the line here
-        std::cout << line << std::endl;
+void run(const string& program) {
+    // Very important function
+    string keyword = "";
+    bool pause_scan = false;
+
+    // Str scanning stuff
+    bool scanning_str = false;
+    string scanned_str = "";
+    int double_quotes_count = 0;
+
+    for (char c : program) { // Checks every character in the .amm file
+        if (keyword == "log") { // Print function
+            pause_scan = true;
+            if (c == '\"') {
+                double_quotes_count++;
+                scanning_str = !scanning_str;
+                continue;
+            }
+            if (scanning_str == true) {
+                scanned_str += c;
+            }
+
+            if (double_quotes_count == 2) {
+                cout << scanned_str << endl;
+
+                scanned_str = "";
+                scanning_str = false;
+                double_quotes_count = 0;
+
+                pause_scan = false;
+
+                continue;
+            }
+        }
+
+        if (pause_scan == false) {
+            keyword += c;
+            //keyword.erase(remove(keyword.begin(), keyword.end(), ' '), keyword.end());
+        }
+
+        if (c == ';') {
+            keyword = "";
+            continue;
+        }
+
+        if (keyword == "whatif") {
+            // Implement the logic for "whatif" keyword
+            // For example, check some conditions and execute code accordingly
+        } else if (keyword == "orelse") {
+            // Implement the logic for "orelse" keyword
+            // For example, execute some other code
+        } else if (keyword == "endif") {
+            // Implement the logic for "endif" keyword
+            // For example, handle the end of a block or condition
+        } else {
+            bool is_known_keyword = false;
+            for (const string& known_keyword : known_keywords) {
+                if (keyword == known_keyword) {
+                    is_known_keyword = true;
+                    break;
+                }
+            }
+
+            //if (!is_known_keyword) {
+            //    cout << " [ERROR]" << endl;
+            //}
         }
     }
-
 }
 
-void run(string file_path) {
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Usage: <compiler path> <filename.amm>" << endl;
+        return 1;
+    }
 
-    string code = get_code(file_path); // Stores the file content in `code` variable
+    string filename = argv[1];
+    string code = get_code(filename);
 
-}
+    // Printing the code content
+    // cout << "Content of " << filename << ":\n";
+    // cout << code << endl;
 
+    run(code);
 
-int main(int argc, char const *argv[]) {
-    // The command to run the program is "gcc main.cpp -lstdc++ -o main.o"
-    get_code(argv[1]);
     return 0;
 }

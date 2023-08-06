27f5aaf70fe98f8f15654e110bb465f57dd12dd5
Abdou990F <benari102@gmail.com>
2023-08-06 17:30:45 +0100

    Added `var`, `getvar` and `getvars`

--- runner.cpp
+++ runner.cpp
@@ -31,6 +31,26 @@ struct Variable {
     string value;
 };
 
+string getVar(vector<Variable> variable_array, int m_address);
+
+
+
+
+// Tools funcs
+
+// TODO: Add a function that takes an array of variables and resets each one
+void reset_props();
+
+// To check if an array has a certain element inside it
+bool strArrHas(vector<string> da_array, string what); // TODO: Add This strArrHas func
+
+/*bool varExists(vector<Variable> variable_array, string var_name) {
+    return find(variable_array.begin(), variable_array.end(), var_name) != variable_array.end();
+}*/
+
+
+// Some checking funcs
+
 bool isInteger(const std::string& s) {
     try {
         size_t pos;
@@ -90,6 +110,9 @@ bool isValidValueForType(const string& value, const string& type) {
     }
 }
 
+
+// A-- functions to work
+
 string get_code(const string& file_path) {
     ifstream program_file(file_path);
     if (!program_file.is_open()) {
@@ -99,6 +122,16 @@ string get_code(const string& file_path) {
     string code((istreambuf_iterator<char>(program_file)), {});
     return code;
 }
+
+// Variable funcs
+
+string getVarValue(vector<Variable> variable_array, int m_address) {
+    string toReturn = variable_array[m_address].value;
+    if (toReturn != "") return toReturn;
+    else return "NULL";
+}
+
+
 
 void run(const string& program) {
     string keyword = "";
@@ -114,51 +147,93 @@ void run(const string& program) {
     
     string var_name;
     string var_type;
-    bool scanning_var_name = true;
+    string var_value;
+    int space_count = 0;
+    bool scanning_var_name = false;
+    bool scanned_var_name = false;
+    string varNextScan = "name";
+    enum VarNextScan {
+        VAR_NAME,
+        VAR_TYPE,
+        VAR_VALUE
+    };
+
 
     for (char c : program) {
         if (keyword == "var") { // Handle variable declarations
+
+            // TODO: Add that variables can either be declared without type but with value or without value but with type
+            // TODO: Add `getvar` keyword to get variable base on its M-Address
+
             pause_scan = true;
 
             // Skip white spaces and move to variable name
-            if (c == ' ') {
-                continue;
+            if ((c == ' ' || c == '\n') && scanning_var_name == false && scanned_var_name == false) {
+                scanning_var_name = true;
+                varNextScan = "name";
             }
 
             // Scan variable name
             if (scanning_var_name == true) {
-                if (c == ' ') {
+                if (c == ':' || c == '=') {
                     scanning_var_name = false;
+                    scanned_var_name = true;
+
+                    if (c == ':') varNextScan = "type";
+                    if (c == '=') varNextScan = "value";
+
+                } else if (c == ' ' || c == '\n') {
+                    continue;
                 } else {
                     var_name += c;
                 }
-            }
-
-            // Skip white spaces and move to variable type
-            if (c == ' ') {
                 continue;
             }
 
             // Scan variable type
-            if (c != ' ') {
+            if (c != ';' && varNextScan == "type") {
+                if (c == '=') {
+                    varNextScan = "value";
+                    continue;
+                }
+                if (c == ' ' || c == '\n') continue;
                 var_type += c;
+                continue;
             }
 
-            cout << "Var: " << var_name << " | Type: " << var_type << endl;
+            if (c != ';' && varNextScan == "value") {
+                if (c == ' ' || c == '\n') continue;
+                var_value += c;
+                continue;
+            }
+
+            //cout << " - Var: " << var_name << " | Type: " << var_type << endl;
 
             // Add the variable to the list
             Variable new_var;
             new_var.name = var_name;
-            new_var.type = var_type;
-            new_var.value = ""; // Initialize with an empty value
+            if (var_type != "") new_var.type = var_type;
+            if (var_value != "") new_var.value = var_value; // Initialize with an empty value (coming soon [; )
             variables.push_back(new_var);
 
+            // Resets the var scanning properties
+            var_name = "";
+            var_type = "";
+            var_value = "";
+            space_count = 0;
+            scanning_var_name = false;
+            scanned_var_name = false;
+
             keyword = ""; // Reset the keyword
-            continue;
+
+            pause_scan = false;
         }
 
         if (keyword == "log") { // Handle log statements
             pause_scan = true;
+
+            bool new_line = true;
+
             if (c == '\"') {
                 double_quotes_count++;
                 scanning_str = !scanning_str;
@@ -168,7 +243,13 @@ void run(const string& program) {
                 scanned_str += c;
             }
             if (double_quotes_count == 2) {
-                cout << scanned_str << endl;
+
+                if (c == '-') {
+                    new_line = false;
+                }
+
+                if (new_line == true) cout << scanned_str << endl;
+                else cout << scanned_str;
 
                 scanned_str = "";
                 scanning_str = false;
@@ -187,18 +268,41 @@ void run(const string& program) {
             // For simplicity, we'll skip this part in this example.
         }
 
-        // Existing code...
+        if (keyword == "getvar") {
+            pause_scan = true;
+
+            int m_address;
+            string varValue;
+
+            if (c == ' ') continue;
+
+            m_address = (int) c;
+            varValue = getVarValue(variables, 0);
+            cout << varValue << endl;
+
+            pause_scan = false;
+        }
+
+        // DEV
+        if (keyword == "getvars") {
+            for (int i = 0; i < sizeof(variables); i++) {
+                cout << variables[i].name << ": " << variables[i].type << " = " << variables[i].value << endl;
+            }
+        }
 
         if (pause_scan == false && c != ';' && c != '\n' && c != ' ') {
             keyword += c;
         }
 
-        if (c == ';') {
+        if (c == ';') { // TODO: Add comments
             keyword = "";
             continue;
         }
     }
 }
+
+
+// So simple... The main funciton... hahaha
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -209,7 +313,7 @@ int main(int argc, char* argv[]) {
     string filename = argv[1];
     string code = get_code(filename);
 
-    run(code);
+    run(code); // This is where EVERYTHING happens
 
     return 0;
 }
